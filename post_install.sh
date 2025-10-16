6d28649a1a8aab1c7a5c3ceb94d3f467dc44fc98
flyovertherainbow <46816271+flyovertherainbow@users.noreply.github.com>
2025-10-16 14:56:54 +1300

    Update and rename post_install.sh to remove_post_install.sh

--- post_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "--- Running Playwright install script ---"
-# Install only the Chromium browser, matching your packages.txt
-python -m playwright install chromium
-echo "--- Playwright install finished ---"
